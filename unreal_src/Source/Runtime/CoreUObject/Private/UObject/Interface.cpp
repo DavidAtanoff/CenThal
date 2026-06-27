57f57ff9fefcca47b2681a2f612b3a844d53911e
DavidAtanoff <atanoff.david@gmail.com>
2026-06-27 19:50:14 -0400

    added ubt and some other minor shit

--- unreal_src/Source/Runtime/CoreUObject/Private/UObject/Interface.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UObject/Interface.h"
-#include "UObject/Class.h"
-
-#include UE_INLINE_GENERATED_CPP_BY_NAME(Interface)
-
--- unreal_src/Source/Runtime/CoreUObject/Private/VerseVM/VVMPackageTypes.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "VerseVM/VVMPackageTypes.h"
-
-#include UE_INLINE_GENERATED_CPP_BY_NAME(VVMPackageTypes)
